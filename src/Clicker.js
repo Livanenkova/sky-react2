4d0e1e2ce673ead1fa7e9eb83faa4737a618115a
Livanenkova <arina909@yandex.ru>
2022-05-16 22:34:07 +0300

    classwork

---
+++ src/Clicker.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+/* eslint-disable react/button-has-type */
+import React from 'react'
+
+class Clicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { countSubmit: 0, countClick: 1 }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleSubmit = (e) => {
+    console.log(e)
+    e.preventDefault()
+    this.setState((prevState) => ({ countSubmit: prevState.countSubmit + 1 }))
+  }
+
+  handleClick(e) {
+    console.log(e)
+    this.setState((prevState) => ({ countClick: prevState.countClick + 1 }))
+  }
+
+  render() {
+    // console.log(this.state.countClick)
+    // console.log(this.state.countSubmit)
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <button>{`${this.state.countSubmit} clicks`}</button>
+        </form>
+        <form>
+          <button type="button" onClick={this.handleClick}>
+            {`${this.state.countClick} clicks`}
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Clicker
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ReacTimer from './ReacTimer';
+import Clicker from './Clicker';
+import ClickerFn from './ClickerFn';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ReacTimer/>
+    <Clicker/>
+    <ClickerFn/>
   </React.StrictMode>
 );
 
