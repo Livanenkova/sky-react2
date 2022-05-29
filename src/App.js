2bade1bcca301c576bf59abbe7e8b091cb9f74d8
Livanenkova <arina909@yandex.ru>
2022-05-29 15:06:18 +0300

    6 homework

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import './App.css'
-// import StudCard from './StudCard'
-// import ValueInput from './ValueInput'
-// import MaxValueInput from './MaxValueInput'
-// import StudCardClass from './StudCardClass'
-import Order from './components/Order'
+import BookCart from './BookCart'
 
 function App() {
-  return (
-    <Order />
-    // <div className="App">
-    //   <StudCard text="Студент" name="Батон" surname="Хлебников"/>
-    //   <StudCard text="Студент" name="Арина" surname="Ливаненкова" />
-    //   <StudCardClass minValue="2" maxValue="9" />
-    // </div>
-  )
+  return <BookCart />
 }
-
-// <StudCard name={}/> --> React.createElement(StudCard, {})
-
-// function StudCard({ text }) {
-//   return (
-//     <>
-//       <hr />
-//       <div className="card">{text}</div>
-//       <hr />
-//     </>
-//   )
-// }
 
 export default App
---
+++ src/TotalPrice.js
@@ -0,0 +1,11 @@
+function TotalPrice({booksCart}) {
+
+  return (
+    <div>
+    <h3>Total: {booksCart}</h3>
+  </div>
+
+  )
+}
+
+export default TotalPrice
