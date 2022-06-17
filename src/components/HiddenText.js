d9cc1bbeec0b00c686c37092a9daf8eab36b99c2
Livanenkova <arina909@yandex.ru>
2022-06-18 00:06:20 +0300

    add homework

---
+++ src/components/HiddenText.js
@@ -0,0 +1,32 @@
+import  { useState} from "react";
+
+const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, in quo! Itaque excepturi magnam in ipsum odit aperiam harum? Consequatur corrupti quisquam dolores recusandae eligendi aut, ipsum aperiam repellendus cumque.'
+
+function HiddenText() {
+  const [textContent, changeText] = useState(text);
+  const [textButton,ChangeTextButton] = useState('Скрыть')
+
+  function submitButton(){
+    if(textButton === 'Скрыть'){
+      ChangeTextButton('Показать')
+    } else if (textButton === 'Показать') {
+      ChangeTextButton('Скрыть')
+    }
+    
+    if(textContent === text) {
+      changeText('')
+    } else if(textContent === '') {
+      changeText(text)
+    }
+  }
+  
+    return (
+      <div>
+        
+        <button type="button" onClick={submitButton}>{textButton}</button>
+        <p>{textContent}</p>
+      </div>
+    )
+}
+
+export default HiddenText
